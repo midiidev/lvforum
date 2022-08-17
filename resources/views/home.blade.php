63323a8c6056c7e2ca020ae3cee9f13a041837cc
Fire-Ash <fire.ash9696@gmail.com>
2022-08-17 12:45:02 -0400

    hide 'create post' button for guest users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,10 @@
 <x-app title="Home">
     <div class="mt-20 max-w-2xl mx-auto space-y-6">
+        @auth
         <p class="text-right">
             <a href="/create-post" class="bg-indigo-600 p-4 rounded-xl"><i class="fa-solid fa-plus"></i> Create Post</a>
         </p>
+        @endauth
         @foreach($posts as $post)
             <div class="bg-slate-800 active:bg-slate-700 rounded-xl p-5 hover:cursor-pointer">
                 <a href="/posts/{{ $post->id }}">
