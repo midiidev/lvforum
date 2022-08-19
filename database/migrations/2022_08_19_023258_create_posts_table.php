900b551e01b3ac5bc112f430d9e4225b204efe5c
Fire-Ash <fire.ash9696@gmail.com>
2022-08-19 17:35:55 -0400

    really fix migrations now (i hate mysql)

--- database/migrations/2022_08_19_023258_create_posts_table.php
+++ database/migrations/2022_08_19_023258_create_posts_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->string('title');
             $table->text('body');
             $table->timestamps();
--- database/migrations/2022_08_19_165907_create_comments_table.php
+++ database/migrations/2022_08_19_165907_create_comments_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->text('body');
             $table->timestamps();
         });
