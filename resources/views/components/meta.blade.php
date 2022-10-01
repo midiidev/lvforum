92c364924e14982d4a1cd71d2fa0d8e7cfcbcd9d
Ash <101067949+Fire-Ash@users.noreply.github.com>
2022-10-01 15:32:24 -0400

    small fix for the previous small fix

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -1,18 +1,18 @@
 <!-- Primary Meta Tags -->
-<title>{{ $title }}</title>
-<meta name="title" content="{{ $title }}">
-<meta name="description" content="{{ $description }}">
+<title>{!! html_entity_decode($title) !!}</title>
+<meta name="title" content="{!! html_entity_decode($title) !!}">
+<meta name="description" content="{!! html_entity_decode($description) !!}">
 
 <!-- Open Graph / Facebook -->
 <meta property="og:type" content="website">
-<meta property="og:url" content="{{ $url }}">
-<meta property="og:title" content="{{ $title }}">
-<meta property="og:description" content="{{ $description }}">
-<meta property="og:image" content="{{ $image }}">
+<meta property="og:url" content="{!! html_entity_decode($url) !!}">
+<meta property="og:title" content="{!! html_entity_decode($title) !!}">
+<meta property="og:description" content="{!! html_entity_decode($description) !!}">
+<meta property="og:image" content="{!! html_entity_decode($image) !!}">
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="{{ $url }}">
-<meta property="twitter:title" content="{{ $title }}">
-<meta property="twitter:description" content="{{ $description }}">
-<meta property="twitter:image" content="{{ $image }}">
+<meta property="twitter:url" content="{!! html_entity_decode($url) !!}">
+<meta property="twitter:title" content="{!! html_entity_decode($title) !!}">
+<meta property="twitter:description" content="{!! html_entity_decode($description) !!}">
+<meta property="twitter:image" content="{!! html_entity_decode($image) !!}">
