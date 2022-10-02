7bb4ba4bb2acf023f20abb264a1658f7dac70ce5
Ash <101067949+Fire-Ash@users.noreply.github.com>
2022-10-02 10:17:50 -0400

    change username and email collation to utf8_bin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
+            $table->string('username')->collation('utf8_bin')->unique();
+            $table->string('email')->collation('utf8_bin')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->text('icon')->nullable();
             $table->string('password');
